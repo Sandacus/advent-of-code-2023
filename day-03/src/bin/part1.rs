465f44f92b7b3efef06cc3e74a1b470cc60e07f8
Sandy Lamond <sandyarl329@gmail.com>
2023-12-16 17:23:03 +0000

    went back to day 3 and completed part 1

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -1,378 +1,131 @@
-use std::collections::{HashMap, BTreeMap};
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::path::Path;
+
+const NOT_SYMBOLS: [char; 11] = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 fn main() {
     println!("Hello day 3!");
 
-    let (engine_schematic, num_lines, line_width) = get_input("test3.txt");
-    println!("number of lines: {},\nline width: {}", num_lines, line_width);
-    println!("The engine schematic looks like this => {:?}", engine_schematic);
+    let input = get_input("src/data/input1.txt");
+    println!("{:?}", input);
 
-    let input: (Vec<String>, usize, usize) = (engine_schematic, num_lines, line_width);
-    let answer: String = part1(input);
-    println!("The answer is: {:?}", answer)
+    let ans = part1(input);
+    println!("The answer is: {}", ans); // 547977 incorrect
 }
 
 
-fn get_input(file_name: &str) -> (Vec<String>, usize, usize) {
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .filter(|l| l != "")
+        .collect()
+}
 
-    // return line width, and number of lines
-    
-
-    let mut file_path: String = std::env::current_dir()
-                                    .unwrap()
-                                    .into_os_string()
-                                    .into_string()
-                                    .unwrap();
-
-    file_path = file_path + "/" + file_name;
-
-    let input_vec: Vec<String> = std::fs::read_to_string(file_path)
-        .unwrap()
-        .lines()
-        .map(String::from)
-        .collect();
-
-    let line_width: usize = input_vec[0].len();
-    
-    let num_lines: usize = input_vec.len();
-    
-    return (input_vec, line_width, num_lines);
+fn get_input(path: &str) -> Vec<Vec<char>> {
+    let contents = lines_from_file(path);
+    contents.iter()
+        .map(|s| s.chars().collect())
+        .collect()
 }
 
 
-fn part1(input: (Vec<String>, usize, usize)) -> String {
-    
-    /*
-    Algorithm design
+fn part1(input: Vec<Vec<char>>) -> i64 {
+    let engine_schematic = input.clone();
 
-    Edge location
-    .....
-    1....
-    .....
-    __     _    __
-    .....|1....|.....
-    allowable places behind: index-(line width -1), index-linewidth
-    allowable places in front: index+1, index+linewidth, index+linewidth+1
+    let part_numbers = find_part_nums(engine_schematic);
 
-    Non-edge location
-    .....
-    ..1..
-    .....
-     ___   _ _   ___
-    .....|..1..|.....
-    allowable places behind: index-1, index-(linewidth-1), index-linewidth, index-(linewidth+1)
-    allowable places in front: index+1, index+(linewidth-1), n+linewidth, n+(linewidth+1)
+    part_numbers.iter().sum() // dummy return
+}
 
-
-    1) Create a hashmap of all numbers using number as value and index as the key.
-    2) Apply the rules for looking up the characters surrounding the numbers to see if they are allowed.
-    3) Sum allowed numbers.
-
-    */
-
-
-    // let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-    let symbols = ['*', '#', '$', '+', '%', '/', '-', '@', '=', '!', '&'];
-    let not_symbols = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-
-    // unpack input
-    let engine_schematic: Vec<String> = input.0;
-    let mut engine_schematic_chars: Vec<char> = Vec::new();
-    let num_lines = input.1;
-    let line_width: usize= input.2;
-
-    // create vector of all characters
-    for line in engine_schematic {
-        for c in line.chars() {
-            engine_schematic_chars.push(c);
-        }
-    }
-
-    // pick out all numbers from engine schematic
-    let mut parts: Vec<char> = Vec::new();
-    let mut parts_map: BTreeMap<usize, char> = BTreeMap::new();
-    let mut idx: usize = 0;
-    for c in &engine_schematic_chars {
-        if numbers.contains(&c) {
-            parts.push(*c);
-            parts_map.insert(idx, *c);
-        }
-        idx += 1;
-    }
-
-    // insert dummy idx at the end 
-    parts_map.insert(idx+1, 'X');
-
-    println!("The parts map is: {:?}", parts_map);
-
-    // combine numbers with keys that are next to each other 
-    // as long as they are on the same "line" i.e., their index / line width (rounded down) is equal
-    let mut previous_key: usize = 0;
-    // let mut next_key: usize = *parts_map.keys().nth(1).unwrap();
-    let mut number_builder: String = String::new();
-    let mut idx_builder: Vec<usize> = Vec::new();
-    let mut part_numbers: HashMap<String, Vec<usize>> = HashMap::new();
-    for (key, val) in parts_map {
-        // println!("key {}: value {}", key, val);
-
-        // check to see if we can combine numbers
-        if key == previous_key {
-            // combine numbers
-            number_builder.push(val);
-            idx_builder.push(key);
-        } else {
-            if (key)/line_width == (previous_key)/line_width && (key-1 == previous_key) {
-                // combine numbers
-                number_builder.push(val);
-                idx_builder.push(key);
-            } else {
-                // add number from number builder as key and index vector as value to part_numbers map
-                part_numbers.insert(number_builder.clone(), idx_builder.clone());
-    
-                // reset number builder and index builder
-                number_builder = String::new();
-                number_builder.push(val);
-                idx_builder = Vec::new();
-                idx_builder.push(key);
-    
-            }
-            
-            previous_key = key;
-        }
-    }
-        
-
-    println!("The part numbers are: {:?}", part_numbers);
-
-
-    //=====================================================
-    // TEST NUMBERS
-    //=====================================================
-    // go through parts numbers to find the valid ones
-    // use the array for each part number and the equations to find the 
-    // indices to check in the character vector
-
-    let mut allowed_part_numbers: Vec<String> = Vec::new();
-
-    for (key, value) in part_numbers {
-        for index in value {
-            
-            // check if edge location
-            if index % line_width == 0 || (index + 1) % line_width == 0 {
-                
-                if index < line_width { // on first line
-                    println!("this is edge location on first line with index {} and character {}",(index), engine_schematic_chars[index]);
-                    if index%line_width == 0 {
-                        // then at start
-                        if !(not_symbols.contains(&engine_schematic_chars[index+1])) {
-                            allowed_part_numbers.push(key);
-                            break;
-                        }
-                    } else {
-                        // at end
-                        if !(not_symbols.contains(&engine_schematic_chars[index-1])) {
-                            allowed_part_numbers.push(key);
-                            break;
-                        }
-                    }
-                    //forwards
-                    if !(not_symbols.contains(&engine_schematic_chars[index+line_width-1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }  
-                    if !(not_symbols.contains(&engine_schematic_chars[index+line_width])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    } 
-                    if !(not_symbols.contains(&engine_schematic_chars[index+line_width+1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
-                } else if index / line_width + 1 == num_lines { // on last line
-                    println!("this is edge location on last line with index {} and character {}",(index), engine_schematic_chars[index]);
-                    if index%line_width == 0 {
-                        // then at start
-                        if !(not_symbols.contains(&engine_schematic_chars[index+1])) {
-                            allowed_part_numbers.push(key);
-                            break;
-                        }
-                    } else {
-                        // at end
-                        if !(not_symbols.contains(&engine_schematic_chars[index-1])) {
-                            allowed_part_numbers.push(key);
-                            break;
-                        }
-                    }
-                    // backwards
-                    if !(not_symbols.contains(&engine_schematic_chars[index-line_width-1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }  
-                    if !(not_symbols.contains(&engine_schematic_chars[index-line_width])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    } 
-                    if !(not_symbols.contains(&engine_schematic_chars[index-line_width+1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
-                } else { // middle line
-                    println!("this is edge location in a middle line with index {} and character {}",(index), engine_schematic_chars[index]);
-                    // test for start or end
-                    if index%line_width == 0 {
-                        // then at start
-                        if !(not_symbols.contains(&engine_schematic_chars[index+1])) {
-                            allowed_part_numbers.push(key);
-                            break;
-                        }
-                    } else {
-                        // at end
-                        if !(not_symbols.contains(&engine_schematic_chars[index-1])) {
-                            allowed_part_numbers.push(key);
-                            break;
-                        }
-                    }
-                    // forwards
-                    if !(not_symbols.contains(&engine_schematic_chars[index+line_width-1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }  
-                    if !(not_symbols.contains(&engine_schematic_chars[index+line_width])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    } 
-                    if !(not_symbols.contains(&engine_schematic_chars[index+line_width+1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
-                    // backwards
-                    if !(not_symbols.contains(&engine_schematic_chars[index-line_width-1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }  
-                    if !(not_symbols.contains(&engine_schematic_chars[index-line_width])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    } 
-                    if !(not_symbols.contains(&engine_schematic_chars[index-line_width+1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
+fn find_part_nums(es: Vec<Vec<char>>) -> Vec<i64> {
+    let numbers: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let mut val_nums: Vec<i64> = Vec::new();
+    // filter out non-part numbers from engine schematic
+    for i in 0..es.len() {
+        let mut j_end = 0;
+        for j in 0..es[0].len() {
+            // skip to next number
+            if j < j_end { continue; }
+            // is es[i][j] a number?
+            if numbers.contains(&es[i][j]) {
+                let mut idx = j;
+                let j_start = j;
+                // are items next to it also numbers?
+                while numbers.contains(&es[i][idx]) {
+                    idx += 1;
+                    if idx > es[i].len() - 1 { break; }
                 }
-
-            } else { // non-edge location
-                if index < line_width { // on first line
-                    println!("this is mid location on first line with index {} and character {}",(index), engine_schematic_chars[index]);
-                    //test backwards
-                    if !(not_symbols.contains(&engine_schematic_chars[index-1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
-                    //test forwards
-                    if !(not_symbols.contains(&engine_schematic_chars[index+1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
-                    if !(not_symbols.contains(&engine_schematic_chars[index+line_width-1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }  
-                    if !(not_symbols.contains(&engine_schematic_chars[index+line_width])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    } 
-                    if !(not_symbols.contains(&engine_schematic_chars[index+line_width+1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
-                } else if index / line_width + 1 == num_lines { // on last line
-                    println!("this is mid location on last line with index {} and character {}",(index), engine_schematic_chars[index]);
-                    //backwards
-                    if !(not_symbols.contains(&engine_schematic_chars[index-1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
-                    if !(not_symbols.contains(&engine_schematic_chars[index-line_width-1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }  
-                    if !(not_symbols.contains(&engine_schematic_chars[index-line_width])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    } 
-                    if !(not_symbols.contains(&engine_schematic_chars[index-line_width+1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
-                    //forwards
-                    if !(not_symbols.contains(&engine_schematic_chars[index+1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
-                } else { // middle line
-                    println!("this is mid location in a middle line with index {} and character {}",(index), engine_schematic_chars[index]);
-                    //test forwards
-                    if !(not_symbols.contains(&engine_schematic_chars[index+1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
-                    if !(not_symbols.contains(&engine_schematic_chars[index+line_width-1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }  
-                    if !(not_symbols.contains(&engine_schematic_chars[index+line_width])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    } 
-                    if !(not_symbols.contains(&engine_schematic_chars[index+line_width+1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
-                    //test backwards
-                    if !(not_symbols.contains(&engine_schematic_chars[index-1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
-                    if !(not_symbols.contains(&engine_schematic_chars[index-line_width-1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }  
-                    if !(not_symbols.contains(&engine_schematic_chars[index-line_width])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    } 
-                    if !(not_symbols.contains(&engine_schematic_chars[index-line_width+1])) {
-                        allowed_part_numbers.push(key);
-                        break;
-                    }
+                j_end = idx;
+                // check number is valid
+                let valid_number = check_number(i, j_start, j_end, &es);
+                // build the number
+                let num = build_number(i, j_start, j_end, &es);
+                println!("Digit is: {} => validity: {}", num, valid_number);
+                if valid_number {
+                    val_nums.push(num);
                 }
             }
         }
     }
-
-    println!("The allowed numbers are {:?}", allowed_part_numbers);
-    let scores: usize = allowed_part_numbers.iter().map(|s| s.parse::<usize>().unwrap()).sum();
-
-    // let mut scores = 0;
-    // for num in allowed_part_numbers {
-    //     scores = scores + num.parse::<i32>().unwrap();
-    // }
-    // equals: 256307
-    scores.to_string()
+    val_nums
 }
 
+fn build_number(row: usize, col_start: usize, col_end: usize, es: &Vec<Vec<char>>) -> i64 {
+    let mut num_chars = vec!['0'; col_end - col_start];
+    num_chars.clone_from_slice(&es[row][col_start..col_end]);
 
-#[cfg(test)]
-mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    use super::*;
+    let num_string: String = num_chars.into_iter().collect();
+    println!("Inside build number. Num to build is: {}", num_string);
 
-    #[test]
-    fn test_get_sum_of_ids() {
-        let result = 8;
-        let answer = 8;
-        assert_eq!(result, answer);
+    let num: i64 = num_string.parse::<i64>().unwrap();
+    num // return
+}
+
+fn check_number(row: usize, mut col_start: usize, col_end: usize, es: &Vec<Vec<char>>) -> bool {
+    // assume false to start with and check for truth
+    let mut number_check = false;
+
+    // check left
+    if col_start > 0 {
+        let ch = es[row][col_start - 1];
+        number_check = if !NOT_SYMBOLS.contains(&ch) { true } else { false }
     }
+    if number_check { return number_check; }
+
+    // check right
+    if col_end < es[row].len() {
+        let ch = es[row][col_end];
+        println!("Inside check right. Character to right is: {}", ch);
+        number_check = if !NOT_SYMBOLS.contains(&ch) { true } else { false }
+    }
+    if number_check { return number_check; }
+
+    if col_start > 0 { col_start -= 1 } else { col_start = 0 } // guard against negative numbers
+    // check above
+    if row > 0 {
+        for x in col_start..col_end + 1 {
+            if x < es[row].len() {
+                let ch = es[row - 1][x];
+                number_check = if !NOT_SYMBOLS.contains(&ch) { true } else { false }
+            }
+            if number_check { break; }
+        }
+    }
+    if number_check { return number_check; }
+
+    // check below
+    if row < es.len() - 1 {
+        for x in col_start..col_end + 1 {
+            if x < es[row].len() {
+                let ch = es[row + 1][x];
+                number_check = if !NOT_SYMBOLS.contains(&ch) { true } else { false }
+            }
+            if number_check { break; }
+        }
+    }
+
+    number_check // return
 }
