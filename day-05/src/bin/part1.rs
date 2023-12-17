971d9c4a75014f6d23e8705e0b98d1d6c8e35b27
Sandy Lamond <sandyarl329@gmail.com>
2023-12-17 15:56:55 +0000

    Completed part 1 of day 5

--- day-05/src/bin/part1.rs
+++ day-05/src/bin/part1.rs
@@ -5,15 +5,8 @@ use std::path::Path;
 fn main() {
     println!("Hello, day 5!");
 
-    let path = "./src/data/test1.txt";
+    let path = "./src/data/input1.txt";
     let input = get_input(path);
-    // println!("{:?}", input);
-
-    // let nums: Vec<_> = input
-    //     .into_iter()
-    //     .filter_map(|s| s.replace(" ", "").parse::<i64>().ok())
-    //     .collect();
-
 
     let ans = part1(input);
     println!("The answer is: {:?}", ans);
@@ -27,6 +20,7 @@ fn part1(input: Vec<String>) -> i64 {
         .split_whitespace()
         .map(|n| n.parse::<i64>().unwrap())
         .collect();
+    println!("The seeds are: {:?}", seeds);
 
     // get the maps
     let mut vec_of_maps: Vec<Vec<String>> = Vec::new();
@@ -39,77 +33,91 @@ fn part1(input: Vec<String>) -> i64 {
             map_keys.push(s);
         }
     }
+    vec_of_maps.push(map_keys.clone()); // get last map
+    // println!("The vec of maps: {:?}", vec_of_maps);
 
-    // convert strings to numbers
-    for mut map_key in vec_of_maps {
-        for i in 1..map_key.len() {
-            map_key[i] = map_key[i].split_whitespace().map(|n| n.clone().parse::<i64>()).unwrap().collect();
-            println!("The nums are: {:?}", map_key[i]);
-        }
+    let s2s_maps = Vec::from(&vec_of_maps[1][1..]);
+    let s2f_maps = Vec::from(&vec_of_maps[2][1..]);
+    let f2w_maps = Vec::from(&vec_of_maps[3][1..]);
+    println!("fertilizer to water map: {:?}", f2w_maps);
+    let w2l_maps = Vec::from(&vec_of_maps[4][1..]);
+    let l2t_maps = Vec::from(&vec_of_maps[5][1..]);
+    let t2h_maps = Vec::from(&vec_of_maps[6][1..]);
+    let h2l_maps = Vec::from(&vec_of_maps[7][1..]);
+
+    let mut locations: Vec<i64> = Vec::new();
+    for seed_start in seeds {
+        let mut seed = seed_start.clone();
+        // Journey of a seed from seed -> location
+        println!("======== The seed map ===============");
+        //=============================================
+        println!("============================");
+        // seed to soil
+        seed = start_to_destination(seed, &s2s_maps);
+        println!("Seed to soil map: {}", seed);
+        //=============================================
+        println!("============================");
+        // soil to fertilizer
+        seed = start_to_destination(seed, &s2f_maps);
+        println!("Soil to fertilizer map: {}", seed);
+        //=============================================
+        println!("============================");
+        // fertilizer to water
+        seed = start_to_destination(seed, &f2w_maps);
+        println!("Fertilizer to water map: {}", seed);
+        //=============================================
+        println!("============================");
+        // water to light
+        seed = start_to_destination(seed, &w2l_maps);
+        println!("Water to light map: {}", seed);
+        //=============================================
+        println!("============================");
+        // light to temperature
+        seed = start_to_destination(seed, &l2t_maps);
+        println!("Light to temperature map: {}", seed);
+        //=============================================
+        println!("============================");
+        // temperature to humidity
+        seed = start_to_destination(seed, &t2h_maps);
+        println!("Temperature to humidity map: {}", seed);
+        //=============================================
+        println!("============================");
+        // humidity to location
+        seed = start_to_destination(seed, &h2l_maps);
+        println!("Humidity to location map: {}", seed);
+        //=============================================
+        // push location to locations vector
+        locations.push(seed);
     }
 
-    // Journey of a seed from seed -> location
-    // seed to soil
-    // let seed = seed_to_soil(42, &vec_of_maps);
-    // println!("The soil map: \n{:?}", seed);
-
-    // soil to fertilizer
-    // map = soil_to_fertilizer(map);
-
-    // fertilizer to water
-    // map = fertilizer_to_soil(map);
-
-    // water to light
-    // map = water_to_light(map);
-
-    // light to temperature
-    // map = light_to_temperature(map);
-
-    // temperature to humidity
-    // map = temperature_to_light(map);
-
-    // humidity to location
-    // map = humidity_to_location(map);
-
-    42
+    locations.iter().min().unwrap().clone() // return
 }
 
-fn humidity_to_location(map: Vec<i64>) -> Vec<i64> {
-    map
-}
-
-fn temperature_to_light(map: Vec<i64>) -> Vec<i64> {
-    map
-}
-
-fn light_to_temperature(map: Vec<i64>) -> Vec<i64> {
-    map
-}
-
-fn water_to_light(map: Vec<i64>) -> Vec<i64> {
-    map
-}
-
-fn fertilizer_to_soil(map: Vec<i64>) -> Vec<i64> {
-    map
-}
-
-fn soil_to_fertilizer(map: Vec<i64>) -> Vec<i64> {
-    map
-}
-
-fn seed_to_soil(mut seed: i64, vec_of_maps: &Vec<Vec<String>>) -> i64 {
+fn start_to_destination(mut seed: i64, maps: &Vec<String>) -> i64 {
+    // s2s_maps e.g., ["seed-to-soil map:", "50 98 2", "52 50 48", ... n]
+    // process s2s_maps
+    let nums = map_to_num(maps);
     // loop over lines in the map to modify seed accordingly
-
-
-    let l1 = vec![50, 98, 2];
-    let mut seed = 10;
-    // first line
-    if (98 <= seed) && (seed < 98 + 2) {
-        seed = seed - 98 + 50;
+    // general formula
+    // if seed within boundary then seed = seed - source + destination
+    for map in nums {
+        if (map[1] <= seed) && (seed < map[1] + map[2]) {
+            seed = seed - map[1] + map[0];
+            break;
+        }
+        println!("Inside start to destination: {:?}", seed);
     }
+    seed // return
+}
 
-    42 // dummy return
+fn map_to_num(map_strings: &Vec<String>) -> Vec<Vec<i64>> {
+    map_strings.iter()
+        .map(|s|
+            s.split_whitespace()
+                .map(|n| n.parse::<i64>().unwrap())
+                .collect()
+            )
+        .collect()
 }
 
 fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
