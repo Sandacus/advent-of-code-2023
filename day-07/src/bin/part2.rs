e4bafeb1d49c863447b00a9ffa1e04987171d39e
Sandy Lamond <sandyarl329@gmail.com>
2023-12-18 22:16:09 +0000

    Completed part 2 for day 7

--- day-07/src/bin/part2.rs
+++ day-07/src/bin/part2.rs
@@ -10,13 +10,13 @@ fn main() {
     let input = get_input(path);
 
     let start = Instant::now();
-    let ans = part1(input);
+    let ans = part2(input);
     println!("The answer is: {:?}", ans);
     let duration = start.elapsed();
     println!("Time elapsed is: {:?}", duration);
 }
 
-fn part1(input: Vec<String>) -> i32 {
+fn part2(input: Vec<String>) -> i32 {
     // make a vector of the hands
     let mut hands: Vec<String> = input
         .iter()
@@ -32,21 +32,19 @@ fn part1(input: Vec<String>) -> i32 {
     for i in 0..hands.len() {
         hands_map.insert(hands[i].clone(), bids[i].parse::<i32>().unwrap());
     }
-
     // rank the hands with bubble sort
-    for i in 0..hands.len()-1 {
+    for i in 0..hands.len() - 1 {
         let mut swapped = false;
-        for j in 0..hands.len()-1-i {
-            if hands[j] == get_higher_rank(&hands[j], &hands[j+1]) {
+        for j in 0..hands.len() - 1 - i {
+            if hands[j] == get_higher_rank(&hands[j], &hands[j + 1]) {
                 let dummy = hands[j].clone();
-                hands[j] = hands[j+1].clone();
-                hands[j+1] = dummy;
+                hands[j] = hands[j + 1].clone();
+                hands[j + 1] = dummy;
                 swapped = true;
             }
         }
-        if !swapped { break }
+        if !swapped { break; }
     }
-
     let mut winnings = 0;
     for (i, hand) in hands.iter().enumerate() {
         winnings += hands_map.get(hand).unwrap() * (i as i32 + 1);
@@ -64,13 +62,15 @@ fn get_higher_rank(hand1: &String, hand2: &String) -> String {
     } else if score1 < score2 {
         return hand2.clone();
     } else if score1 == score2 {
-        return decider(hand1, hand2)
+        return decider(hand1, hand2);
     }
 
     panic!("Can't find a higher rank in fn get_higher_rank!") //return
 }
 
 fn get_hand_rank(hand: &String) -> i32 {
+    let mut joker_count = 0;
+
     let mut card_count = HashMap::new();
     card_count.insert('2', 0);
     card_count.insert('3', 0);
@@ -81,13 +81,13 @@ fn get_hand_rank(hand: &String) -> i32 {
     card_count.insert('8', 0);
     card_count.insert('9', 0);
     card_count.insert('T', 0);
-    card_count.insert('J', 0);
     card_count.insert('Q', 0);
     card_count.insert('K', 0);
     card_count.insert('A', 0);
 
     for card in hand.chars() {
         match card {
+            'J' => joker_count += 1,
             '2' => *card_count.entry('2'.to_owned()).or_default() += 1,
             '3' => *card_count.entry('3'.to_owned()).or_default() += 1,
             '4' => *card_count.entry('4'.to_owned()).or_default() += 1,
@@ -97,7 +97,6 @@ fn get_hand_rank(hand: &String) -> i32 {
             '8' => *card_count.entry('8'.to_owned()).or_default() += 1,
             '9' => *card_count.entry('9'.to_owned()).or_default() += 1,
             'T' => *card_count.entry('T'.to_owned()).or_default() += 1,
-            'J' => *card_count.entry('J'.to_owned()).or_default() += 1,
             'Q' => *card_count.entry('Q'.to_owned()).or_default() += 1,
             'K' => *card_count.entry('K'.to_owned()).or_default() += 1,
             'A' => *card_count.entry('A'.to_owned()).or_default() += 1,
@@ -108,14 +107,27 @@ fn get_hand_rank(hand: &String) -> i32 {
     // get rid of zeros
     card_count.retain(|_, &mut v| v != 0);
 
-    let max_cards = card_count.values().max().unwrap();
-    let min_cards = card_count.values().min().unwrap();
+    let cards_count = card_count.values().max();
+    let max_cards: i32;
+    if cards_count.is_some() {
+        max_cards = joker_count + cards_count.unwrap();
+    } else {
+        max_cards = joker_count;
+    }
+
+    let min_cards_count = card_count.values().min();
+    let min_cards: i32;
+    if cards_count.is_some() {
+        min_cards = min_cards_count.unwrap().clone();
+    } else {
+        min_cards = 0;
+    }
 
     // Now test for the type of hand
     let hand_rank = match max_cards {
         5 => 7,
         4 => 6,
-        3 => if *min_cards == 2 { 5 } else { 4 },
+        3 => if min_cards == 2 { 5 } else { 4 },
         2 => if card_count.len() == 3 { 3 } else { 2 },
         1 => 1,
         _ => panic!(),
@@ -133,7 +145,7 @@ fn decider(hand1: &String, hand2: &String) -> String {
             return hand1.clone();
         } else if card_value(cards1[i]) < card_value(cards2[i]) {
             return hand2.clone();
-        } else { continue }
+        } else { continue; }
     }
     println!("cards 1: {:?} vs cards 2: {:?}", cards1, cards2);
     panic!("Couldn't decide a higher hand in fn decider!") // return
@@ -141,6 +153,7 @@ fn decider(hand1: &String, hand2: &String) -> String {
 
 fn card_value(card: char) -> i32 {
     match card {
+        'J' => 1,
         '2' => 2,
         '3' => 3,
         '4' => 4,
@@ -150,7 +163,6 @@ fn card_value(card: char) -> i32 {
         '8' => 8,
         '9' => 9,
         'T' => 10,
-        'J' => 11,
         'Q' => 12,
         'K' => 13,
         'A' => 15,
